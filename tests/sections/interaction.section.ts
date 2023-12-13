a4d4769b19a1cb30f40d888add35a91e300f3231
VitGT <tvit@mail.ru>
2023-12-13 11:29:14 +0500

    add first test for interactions pages

--- tests/sections/interaction.section.ts
+++ tests/sections/interaction.section.ts
@@ -3,35 +3,37 @@ import { type Page, type Locator, expect } from "@playwright/test";
 export class InteractionsSection {
   readonly page: Page;
   readonly rootElement: Locator;
+  readonly sortableElement: Locator;
+  readonly selectableElement: Locator;
+  readonly resizableElement: Locator;
+  readonly droppableElement: Locator;
+  readonly dragabbleElement: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.rootElement = page.locator('span').filter({ hasText: 'Interactions' }).locator('div').first()
+    this.rootElement = page.locator('span').filter({ hasText: 'Interactions' }).locator('div').first();
     // this.rootElement = page.locator('span').filter({ hasText: 'Elements' }).locator('div').first()
-
+    this.sortableElement = page.locator('li').filter({ hasText: 'Sortable' });
+    this.selectableElement = page.locator('li').filter({ hasText: 'Selectable' });
+    this.resizableElement = page.locator('li').filter({ hasText: 'Resizable' });
+    this.droppableElement = page.locator('li').filter({ hasText: 'Droppable' });
+    this.dragabbleElement = page.locator('li').filter({ hasText: 'Dragabble' });
   }
 
-
-  async isOpen(){
-    // await expect(this.textBoxElement).toBeVisible();
-    // await expect(this.checkBoxElement).toBeVisible();
-    // await expect(this.radioButtonElement).toBeVisible();
-    // await expect(this.webTablesElement).toBeVisible();
-    // await expect(this.buttonsElement).toBeVisible();
-    // await expect(this.linksElement).toBeVisible();
-    // await expect(this.uploadAddDownloadElement).toBeVisible();
-    // await expect(this.dynamicPropertiesElement).toBeVisible();
+  async isOpen() {
+    await expect(this.sortableElement).toBeVisible();
+    await expect(this.selectableElement).toBeVisible();
+    await expect(this.resizableElement).toBeVisible();
+    await expect(this.droppableElement).toBeVisible();
+    await expect(this.dragabbleElement).toBeVisible();
   }
 
   async isClose() {
-    // await expect(this.textBoxElement).toBeHidden();
-    // await expect(this.checkBoxElement).toBeHidden();
-    // await expect(this.radioButtonElement).toBeHidden();
-    // await expect(this.webTablesElement).toBeHidden();
-    // await expect(this.buttonsElement).toBeHidden();
-    // await expect(this.linksElement).toBeHidden();
-    // await expect(this.uploadAddDownloadElement).toBeHidden();
-    // await expect(this.dynamicPropertiesElement).toBeHidden();
+    await expect(this.sortableElement).toBeHidden();
+    await expect(this.selectableElement).toBeHidden();
+    await expect(this.resizableElement).toBeHidden();
+    await expect(this.droppableElement).toBeHidden();
+    await expect(this.dragabbleElement).toBeHidden();
   }
 }
 
---
+++ tests/ui/interactions.spec.ts
@@ -0,0 +1,24 @@
+// import { test, expect } from '@playwright/test';
+import { test, expect } from "../fixtures/pages.fixture";
+
+test.describe("Interactions section tests", () => {
+  // test.beforeEach(async ({ page, homePage }) => {
+  //   await homePage.load();
+  // });
+
+  test("Interactions page has header", async ({ interactionsPage }) => {
+    // Expect a title "to contain" a substring.
+    await interactionsPage.assertPageHeader();
+  });
+
+  test("'Interactions' accordion section opens", async ({ interactionsPage }) => {
+    await interactionsPage.accordionSection.interactionsSection.isOpen();
+    });
+
+  test("'Interactions' accordion section closes", async ({ interactionsPage }) => {
+    await interactionsPage.accordionSection.interactionsSection.rootElement.click();
+    await interactionsPage.accordionSection.interactionsSection.isClose();
+  });
+
+
+});
