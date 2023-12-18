03728b6ce08e5969ab7c52682a3ce4fe53abe2a6
VitGT <tvit@mail.ru>
2023-12-18 16:56:01 +0500

    experimental checkbox page 1

--- tests/ui-tests/elements-specs/test.example.ts
+++ tests/ui-tests/elements-specs/test.example.ts
@@ -7,9 +7,11 @@ export class CheckboxPage {
   readonly header: Locator;
   readonly expandAllButton: Locator;
   readonly collapseAllButton: Locator;
+  readonly resultMessage: Locator;
 
   readonly checkboxes: Record<string, Locator>; // Изменили тип на объект
   private checkboxIndexByName: Record<string, number>;
+  private statusMessages: Record<string, string>;
 
   constructor(page: Page) {
     this.page = page;
@@ -19,34 +21,51 @@ export class CheckboxPage {
     // Expand and Collapse
     this.expandAllButton = page.getByRole('button', { name: 'Expand all' });
     this.collapseAllButton = page.getByRole('button', { name: 'Collapse all' });
+    this.resultMessage = page.locator('#result');
 
-
-    this.checkboxes = {
-      'Home': page.locator('label').filter({ hasText: 'Home' }),
-      'Desktop': page.locator('label').filter({ hasText: 'Desktop' }),
-      'Notes': page.locator('label').filter({ hasText: 'Notes' }),
-      'Commands': page.locator('label').filter({ hasText: 'Commands' }),
-      'Documents': page.locator('label').filter({ hasText: 'Documents' }),
-      'WorkSpace': page.locator('label').filter({ hasText: 'WorkSpace' }),
-      'React': page.locator('label').filter({ hasText: 'React' }),
-      'Angular': page.locator('label').filter({ hasText: 'Angular' }),
-      'Veu': page.locator('label').filter({ hasText: 'Veu' }),
-      'Office': page.locator('label').filter({ hasText: 'Office' }),
-      'Public': page.locator('label').filter({ hasText: 'Public' }),
-      'Private': page.locator('label').filter({ hasText: 'Private' }),
-      'Classified': page.locator('label').filter({ hasText: 'Classified' }),
-      'General': page.locator('label').filter({ hasText: 'General' }),
-      'Downloads': page.locator('label').filter({ hasText: 'Downloads' }),
-      'Word File.doc': page.locator('label').filter({ hasText: 'Word File.doc' }),
-      'Excel File.doc': page.locator('label').filter({ hasText: 'Excel File.doc' }),
-      // Добавьте другие локаторы по необходимости
-    };
+    // Используем новый метод для создания локаторов чекбоксов
+    this.checkboxes = this.createCheckboxes([
+      'Home', 'Desktop', 'Notes', 'Commands', 'Documents', 'WorkSpace', 'React',
+      'Angular', 'Veu', 'Office', 'Public', 'Private', 'Classified', 'General',
+      'Downloads', 'Word File.doc', 'Excel File.doc'
+    ]);
 
     // Создаем объект с соответствием индексов именам чек-боксов
     this.checkboxIndexByName = {};
     Object.entries(this.checkboxes).forEach(([checkboxName, locator], index) => {
       this.checkboxIndexByName[checkboxName] = index;
     });
+
+    // Статусные сообщения для каждого чекбокса
+    this.statusMessages = {
+      'Home': 'home',
+      'Desktop': 'desktop',
+      'Notes': 'notes',
+      'Commands': 'commands',
+      'Documents': 'documents',
+      'WorkSpace': 'workspace',
+      'React': 'react',
+      'Angular': 'angular',
+      'Veu': 'veu',
+      'Office': 'office',
+      'Public': 'public',
+      'Private': 'private',
+      'Classified': 'classified',
+      'General': 'general',
+      'Downloads': 'downloads',
+      'Word File.doc': 'wordFile',
+      'Excel File.doc': 'excelFile',
+    };
+  }
+
+  private createCheckboxes(checkboxNames: string[]): Record<string, Locator> {
+    const checkboxes: Record<string, Locator> = {};
+
+    checkboxNames.forEach((checkboxName) => {
+      checkboxes[checkboxName] = this.page.locator('label').filter({ hasText: checkboxName });
+    });
+
+    return checkboxes;
   }
 
   getCheckboxByName(checkboxName: string): Locator | undefined {
@@ -57,20 +76,40 @@ export class CheckboxPage {
     return this.checkboxIndexByName[checkboxName];
   }
 
+  async clickCheckbox(checkboxName: string): Promise<void> {
+    const checkbox = this.checkboxes[checkboxName];
+
+    if (checkbox) {
+      await checkbox.click();
+      console.log(`Clicked on checkbox: ${checkboxName}`);
+    } else {
+      console.error('Checkbox not found');
+    }
+  }
+
+  async assertCheckboxStatus(checkboxName: string, isChecked: boolean): Promise<void> {
+    const statusMessage = this.statusMessages[checkboxName];
+    if (statusMessage) {
+      const textContent = await this.resultMessage.textContent();
+      if (textContent !== null && ((isChecked && textContent.includes(statusMessage)) || (!isChecked && !textContent.includes(statusMessage)))) {
+        console.log(`Checkbox status for ${checkboxName}: ${isChecked ? 'checked' : 'unchecked'}`);
+      } else {
+        console.error(`Unexpected checkbox status for ${checkboxName}`);
+      }
+    } else {
+      console.error('Status message not found');
+    }
+  }
+
+
   async clickRandomCheckboxes(count: number): Promise<void> {
     const checkboxNames = Object.keys(this.checkboxes);
 
     // Выбираем случайные чекбоксы
     for (let i = 0; i < count; i++) {
       const randomCheckboxName = checkboxNames[Math.floor(Math.random() * checkboxNames.length)];
-      const randomCheckbox = this.checkboxes[randomCheckboxName];
-
-      if (randomCheckbox) {
-        await randomCheckbox.click();
-        console.log(`Clicked on a random checkbox: ${randomCheckboxName}`);
-      } else {
-        console.error('Random checkbox not found');
-      }
+      await this.clickCheckbox(randomCheckboxName);
+      await this.assertCheckboxStatus(randomCheckboxName, true);
     }
   }
 }
--- tests/ui-tests/elements-specs/test.spec.ts
+++ tests/ui-tests/elements-specs/test.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { CheckboxPage } from './test.example';
 
+test.describe.configure({ mode: 'serial' });
 test.describe.only("Check Box element tests", () => {
     // Случайным образом выбираем несколько чекбоксов (например, 3)
 
@@ -10,21 +11,24 @@ test.describe.only("Check Box element tests", () => {
         checkboxPage.expandAllButton.click();
 
         await checkboxPage.checkboxes['Desktop'].click();
+        await checkboxPage.assertCheckboxStatus('Desktop', true)
         console.log('Clicked on the "Desktop" checkbox');
         await page.waitForTimeout(3000)
-        // Дополнительные проверки или действия, если необходимо
-    // });
 
-    // test('Click on a random checkbox', async ({ page }) => {
-        // const checkboxPage = new CheckboxPage(page);
-        // checkboxPage.page.goto(checkboxPage.url, { waitUntil: 'domcontentloaded' })
-        // checkboxPage.expandAllButton.click();
-
-
-  // Указываем количество чек-боксов для клика
-        const count = 3;
-        await checkboxPage.clickRandomCheckboxes(count);
-        await page.waitForTimeout(3000)
     });
+
+//     test('Click on a random checkbox', async ({ page }) => {
+//         const checkboxPage = new CheckboxPage(page);
+//         checkboxPage.page.goto(checkboxPage.url, { waitUntil: 'domcontentloaded' })
+//         checkboxPage.expandAllButton.click();
+
+
+//   // Указываем количество чек-боксов для клика
+//         const count = 3;
+//         await checkboxPage.clickRandomCheckboxes(count);
+//         await checkboxPage.assertCheckboxStatus('Desktop'.checkboxes, true)
+//         await page.waitForTimeout(3000)
+
+//     });
 
 })
